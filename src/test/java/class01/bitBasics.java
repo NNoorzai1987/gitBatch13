b2a50b66b6851c769c040afdad14a75cd15afa06
Wellstudy1985 <Nnoorzai.001@gmail.com>
2022-08-24 18:30:05 -0700

    hot code

--- src/test/java/class01/bitBasics.java
+++ src/test/java/class01/bitBasics.java
@@ -5,5 +5,6 @@ public class bitBasics {
         //write a code that checks the temperature from a website and then
         //prints out that is it a beautiful day or not
         System.out.println("today is not a  beautiful day");
+        System.out.println("Today is little bit hot");
     }
 }
